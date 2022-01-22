833d3cb80951a29c7c55b3c982037db19daa0545
sfjuocekr <sfjuocekr@gmail.com>
2022-01-22 04:24:42 +0100

    some fixes

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,8 @@
 
 // Latency testing, this will emulate different refresh rates and/or add latency between input and output.
 // Values are milliseconds.
-#define LATENCY 1  // increase to add latency, 1 means no latency and 255 is the maximum.
-#define INTERVAL 1 // increase to change the refresh interval in milliseconds, 1 means every millisecond... USB won't go faster anyway!
+#define LATENCY 0  // increase to add latency, 0 means no latency and 255 is the maximum.
+#define INTERVAL 1 // increase to change the refresh interval in milliseconds, USB won't go faster than 1ms anyway!
 
 // Number of channels
 #define CHANNELS 16
@@ -33,12 +33,13 @@
 
 SBUS sbus(Serial1);
 CrsfSerial crsf(Serial2, 115200);
-IntervalTimer hidTimer, latencyTimer, tlmTimer;
-uint16_t hats[3] = {293, 338, 360};
+IntervalTimer hidTimer, latencyTimer;
+uint8_t crsfbatt[CRSF_FRAME_BATTERY_SENSOR_PAYLOAD_SIZE] = {0, 50, 0, 50, 0, 0, 0, 100}; // fake full 5v battery
+uint16_t hats[3] = {293, 338, 0};
 uint16_t ch_latency[LATENCY + 1][CHANNELS];
 bool failSafe, lostFrame;
 
-void setSticks(int _min = 1000, int _max = 2000)
+void setSticks(uint16_t _min = 1000, uint16_t _max = 2000)
 {
   // Use 0-1024 for min and mix instad of 0-65535 if you use the normal layout in usb_desc.h
   Joystick.X(map(ch_latency[0][0], _min, _max, 0, 65535));       // ROLL
@@ -53,17 +54,17 @@ void setSticks(int _min = 1000, int _max = 2000)
   Joystick.hat(1, hats[map(ch_latency[0][7], _min, _max, 0, 2)]);  // FPV.SkyDive knows about the hat!
 }
 
-void setButton(unsigned _button, int _min = 1000, int _max = 2000)
+void setButton(uint8_t _button, uint16_t _min = 1000, uint16_t _max = 2000)
 {
-  for (unsigned _position = 0; _position < 3; _position++)
+  for (uint8_t _position = 0; _position < 3; _position++)
   {
-    Joystick.button(_button * 3 + _position + 1, (unsigned)map(ch_latency[0][4 + _button], _min, _max, 0, 2) == _position ? 1 : 0);
+    Joystick.button(_button * 3 + _position + 1, (uint8_t)map(ch_latency[0][4 + _button], _min, _max, 0, 2) == _position ? true : false);
   }
 }
 
-void setButtons(int _min = 1000, int _max = 2000)
+void setButtons(uint16_t _min = 1000, uint16_t _max = 2000)
 {
-  for (unsigned _button = 0; _button < (CHANNELS - 4); _button++)
+  for (uint8_t _button = 0; _button < (CHANNELS - 4); _button++)
   {
     setButton(_button, _min, _max);
   }
@@ -71,13 +72,15 @@ void setButtons(int _min = 1000, int _max = 2000)
 
 void packetChannels()
 {
-  for (unsigned _channel = 0; _channel < CHANNELS; _channel++)
+  for (uint8_t _channel = 0; _channel < CHANNELS; _channel++)
   {
-    ch_latency[LATENCY > 1 ? LATENCY - 1 : 0][_channel] = crsf.getChannel(_channel + 1);
+    ch_latency[LATENCY][_channel] = crsf.getChannel(_channel + 1);
   }
 
   setSticks(US_MIN, US_MAX);
   setButtons(US_MIN, US_MAX);
+
+  crsf.queuePacket(CRSF_SYNC_BYTE, CRSF_FRAMETYPE_BATTERY_SENSOR, &crsfbatt, sizeof(crsfbatt));
 }
 
 void onInterval()
@@ -87,48 +90,33 @@ void onInterval()
 
 void induceLatency()
 {
-  if (LATENCY > 1)
+  for (uint8_t _bufs = 0; _bufs < LATENCY; _bufs++)
   {
-    for (uint8_t _bufs = 0; _bufs < LATENCY - 1; _bufs++)
-    {
-      memcpy(ch_latency[_bufs], ch_latency[_bufs + 1], sizeof(uint16_t) * CHANNELS);
-    }
+    memcpy(ch_latency[_bufs], ch_latency[_bufs + 1], sizeof(uint16_t) * CHANNELS);
   }
-}
-
-void onTelemetry()
-{
-  uint8_t crsfbatt[CRSF_FRAME_BATTERY_SENSOR_PAYLOAD_SIZE];
-
-  crsfbatt[0] = 0;
-  crsfbatt[1] = 50; // Fake 5v voltage
-  crsfbatt[2] = 0;
-  crsfbatt[3] = 50; // current
-  crsfbatt[4] = 0;
-  crsfbatt[5] = 0; // capacity
-  crsfbatt[6] = 0;
-  crsfbatt[7] = 100; // Bat%
-
-  crsf.queuePacket(CRSF_SYNC_BYTE, CRSF_FRAMETYPE_BATTERY_SENSOR, &crsfbatt, sizeof(crsfbatt));
 }
 
 void linkUp()
 {
-  digitalWrite(13, HIGH);
-
-  tlmTimer.begin(&onTelemetry, 1000 * 500); // Send telemetry only twice per second
+  digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void linkDown()
 {
-  digitalWrite(13, LOW);
-
-  tlmTimer.end();
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
 void setup()
 {
-  pinMode(13, OUTPUT); // LED to show if CRSF is active
+  for (uint16_t _x = 0; _x <= LATENCY; _x++)
+  {
+    for (uint8_t _y = 0; _y < CHANNELS; _y++)
+    {
+      ch_latency[_x][_y] = 0; // pre-allocate the array with zeros
+    }
+  }
+
+  pinMode(LED_BUILTIN, OUTPUT); // LED to show if CRSF is active
 
   sbus.begin();
 
@@ -138,8 +126,20 @@ void setup()
 
   Joystick.useManualSend(true);
 
+  for (uint8_t _x = 0; _x <= 10; _x++)
+  {
+    // Take a breather, I'm not sure why but I need to delay here or 1ms timers refused to work and the whole thing explodes from tine to time...
+    digitalWrite(LED_BUILTIN, _x % 2 == 0 ? LOW : HIGH);
+    
+    delay(100);
+  }
+
   hidTimer.begin(&onInterval, (INTERVAL == 0 ? 1 : INTERVAL) * 1000);
-  latencyTimer.begin(&induceLatency, 1000); // latency step is one millisecond
+
+  if (LATENCY > 0)
+  {
+    latencyTimer.begin(&induceLatency, 1000); // latency step is one millisecond
+  }
 }
 
 void loop()
@@ -148,7 +148,7 @@ void loop()
 
   if (!crsf.isLinkUp()) // fallback to SBUS
   {
-    if (sbus.read(&ch_latency[LATENCY > 1 ? LATENCY - 1 : 0][0], &failSafe, &lostFrame))
+    if (sbus.read(&ch_latency[LATENCY][0], &failSafe, &lostFrame))
     {
       setSticks(STARTPOINT, ENDPOINT);
       setButtons(STARTPOINT, ENDPOINT);
